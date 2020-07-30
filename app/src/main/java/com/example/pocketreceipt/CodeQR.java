745a6ba65dac4222770f1ee19eea35272aa86ffd
hanoc95 <hannahoconnor95@icloud.com>
2020-07-30 18:29:09 +0100

    update

--- app/src/main/java/com/example/pocketreceipt/CodeQR.java
+++ app/src/main/java/com/example/pocketreceipt/CodeQR.java
@@ -4,9 +4,9 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
@@ -16,34 +16,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class CodeQR extends AppCompatActivity {
 
-    EditText User_txt;
     Button generateBtn;
     ImageView qrImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_code_q_r);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_code_q_r );
 
-        User_txt = (EditText)findViewById(R.id.user_id);
-        generateBtn = (Button)findViewById(R.id.generate_btn);
-        qrImage = (ImageView)findViewById(R.id.imageview);
 
-        generateBtn.setOnClickListener(new View.OnClickListener() {
+        generateBtn = (Button) findViewById( R.id.generate_btn );
+        qrImage = (ImageView) findViewById( R.id.imageview );
+
+        generateBtn.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                //you can put a check here for null user
+                setupUserQRcode( FirebaseAuth.getInstance().getCurrentUser().getUid() );
 
-                try{
-                    BitMatrix bitMatrix = multiFormatWriter.encode(User_txt.getText().toString(),BarcodeFormat.QR_CODE,500,500);
-                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
-                    qrImage.setImageBitmap(bitmap);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
             }
-        });
+        } );
     }
+
+    private void setupUserQRcode(String firebaseUID) {
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode( firebaseUID, BarcodeFormat.QR_CODE, 500, 500 );
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap( bitMatrix );
+            qrImage.setImageBitmap( bitmap );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
